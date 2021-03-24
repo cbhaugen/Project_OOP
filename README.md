b0810b4e6c25d8fd3921b24b62cc173de6461c65
Christy Haugen <cbhaugen@gmail.com>
2021-03-24 09:13:37 -0500

    ttt- player info, set up player turns

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ Project 2: Mastermind
 If you’ve never played Mastermind, a game where you have to guess your opponent’s secret code within a certain number of turns (like hangman with colored pegs), check it out on [Wikipedia](https://en.wikipedia.org/wiki/Mastermind_(board_game)). Each turn you get some feedback about how good your guess was – whether it was exactly correct or just the correct color but in the wrong space.
 
 Assignment 2
+
 Build a Mastermind game from the command line where you have 12 turns to guess the secret code, starting with you guessing the computer’s random code.
 
   1. Think about how you would set this problem up!
---
+++ tic_tac_toe.rb
@@ -0,0 +1,96 @@
+class Board
+
+  attr_reader :PlayGame, :Player
+
+  def initialize()
+    @board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+  end
+
+  def display_board
+    puts "|-----------|"
+    puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |"
+    puts "|---+---+---|"
+    puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |"
+    puts "|---+---+---|"
+    puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
+    puts "|-----------|"
+  end
+
+  def check_board(choice, symbol)
+    if @board.include?(PlayGame.choice)
+      @board[choice - 1] = symbol
+    else
+      puts "Please choose a valid number"
+    end
+  end
+
+end
+
+
+class Player
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+
+  def name
+    @name
+  end
+
+end
+
+
+class PlayGame
+  attr_accessor :Board, :Player
+
+  def initialize
+    @play1turn = true
+  end
+
+  def player_info
+    puts "Player 1, what is your name?"
+    @player1 = Player.new(gets.strip, "X")
+
+    puts "Player 2, what is your name?"
+    @player2 = Player.new(gets.strip, "O")
+
+    puts "#{@player1.name}, you are X."
+    puts "#{@player2.name}, you are O."
+    game_board = Board.new()
+    game_board.display_board
+  end
+
+  def pick_space
+    case @play1turn
+    when true
+      puts "#{@player1.name}, type a number to pick a spot."
+    when false
+      puts "#{@player2.name}, type a number to pick a spot."
+    else
+      puts "error"
+    end
+
+  end
+  
+end
+
+new_game = PlayGame.new()
+new_game.player_info
+new_game.pick_space
+
+#define turn
+  #pick a space
+  #player can't play on taken space
+
+#define when game is over
+  #winning combinations
+    #define top row, middle row, etc
+  #define full board
+  #tie situations
+    #entire board is full but no winning combinations
+
+#game play
+  #display board
+  #player take turn
+  #check for game over (if not, back to display board, if so, puts winner lines)
